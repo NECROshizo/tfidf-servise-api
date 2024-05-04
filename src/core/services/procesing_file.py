c014e4ddafb2273954cc56d0ff1bdb5b6dea7aaa
NECROshizo <necroshizo@gmail.com>
2024-05-04 20:43:18 +0300

    add async redis

--- src/core/services/procesing_file.py
+++ src/core/services/procesing_file.py
@@ -6,22 +6,29 @@ from fastapi import UploadFile
 
 from src.core.redis import redis_tools
 
+CHUNK_SIZE = 1024
 
-async def get_tf_idf(content: UploadFile) -> list[dict[str, Any]]:
+
+async def get_tf_idf(content: UploadFile, output_values: int = 50) -> list[dict[str, Any]]:
 	words_counter = Counter()
 	result = []
-	all_docs = redis_tools.increment_docs()
-	text = await content.read()
 
-	words = re.findall(r"\w+", text.decode().lower())
-	words_counter.update(words)
-	sum_all_words = sum(words_counter.values())
+	async with redis_tools as redis:
+		all_docs = redis.increment_docs()
+		while True:
+			chunk = await content.read(CHUNK_SIZE)
+			if not chunk:
+				break
+			words = re.findall(r"\w+", chunk.decode().lower())
+			words_counter.update(words)
 
-	for word, count in words_counter.items():
-		tf = count / sum_all_words
-		docs_count = redis_tools.increment_or_create(word)
-		idf = math.log(all_docs / docs_count)
+		sum_all_words = sum(words_counter.values())
 
-		result.append({"word": word, "tf": tf, "idf": idf})
+		for word, count in words_counter.items():
+			tf = count / sum_all_words
+			docs_count = redis.increment_or_create(word)
+			idf = math.log(all_docs / docs_count)
 
-	return sorted(result, key=lambda x: (-x["tf"], x["word"]))[:50]
+			result.append({"word": word, "tf": tf, "idf": idf})
+
+	return sorted(result, key=lambda x: (-x["tf"], x["word"]))[:output_values]
